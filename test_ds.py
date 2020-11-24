c2fd7bc5660752fa80621d2a72d4220c967f196b
kevchan7 <kchan9001@gmail.com>
2020-11-24 12:51:06 -0800

    pytest now works for test_ds.py

---
+++ test_ds.py
@@ -0,0 +1,18 @@
+from datastructures import *
+
+
+def test_stack():
+    teststack = Stack([1, 2, 3, 4, 5])
+    assert teststack.offload() == 1
+    assert teststack.offload() == 2
+    teststack.offload()
+    teststack.offload()
+    teststack.offload()
+    assert teststack.list == []
+    teststack.stacking(1)
+    popstack3 = teststack.offload()
+    assert popstack3 == 1
+    assert teststack.list == []
+
+
+test_stack()
