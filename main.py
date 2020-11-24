b59d4d0d984797fc71bb281891c6bed36be2a458
kevchan7 <kchan9001@gmail.com>
2020-11-24 13:09:40 -0800

    when i click run pytest on then function it does not show the typical pytest stuff

--- main.py
+++ main.py
@@ -2,21 +2,22 @@
 from tree_node import tree_node
 
 
-def test_btrees(x,y):
-    assert x == y
+def test_btrees():
+    rooted = tree_node(1)
+    rooted.left = tree_node(4)
+    rooted.right = tree_node(2)
+    rooted.right.right = tree_node(3)
+    print(rooted.preorder_traversal(rooted))
+    assert rooted.preorder_traversal(rooted) == [1, 4, 2, 3]
+    print(rooted.postorder_traversal(rooted))
+    assert rooted.postorder_traversal(rooted) == [4, 3, 2, 1]
+    print(rooted.inorder_traversal(rooted))
+    assert rooted.inorder_traversal(rooted) == [4, 1, 2, 32]
+    rooted.level_order_traversal(rooted)
+test_btrees()
 
 
-rooted = tree_node(1)
-rooted.left = tree_node(4)
-rooted.right = tree_node(2)
-rooted.right.right = tree_node(3)
-print(rooted.preorder_traversal(rooted))
-test_btrees(rooted.preorder_traversal(rooted), [1, 4, 2, 3])
-print(rooted.postorder_traversal(rooted))
-test_btrees(rooted.postorder_traversal(rooted), [4, 3, 2, 1])
-print(rooted.inorder_traversal(rooted))
-test_btrees(rooted.inorder_traversal(rooted), [4, 1, 2, 3])
-rooted.level_order_traversal(rooted)
+
 #rooted.inorderiterative(rooted) given up for now
 
 
